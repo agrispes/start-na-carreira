17d83b062fc5ea157b379f4131ab7e0e9538ea37
Agrispes de Freitas silva <117922473+agrispes@users.noreply.github.com>
2023-03-10 13:34:30 -0300

    fim
    terminei o projeto com algumas inconsistência

--- scripts.js
+++ scripts.js
@@ -3,9 +3,24 @@ event.preventDeFault();
 let bill = document.getElementById("bill").Value;
 let serviceQual = document.getElementById("serviceQual").Value;
 let numOfPeople = document.getElementById("people").Value;
-if (bill =='' ser) {
-    
+if (bill == '' 〡 serviceQual == 0){
+   alert("por favor, preencha os valores") return;
 }
+if(numOfPeople == "" 〡 numOfPeople <= 1) {
+numOfPeople = 1;
+document.getElementById('each').style.display = "none"
+
+} else {
+
+    document.getElementById('each').style.display = "block"
+}
+
+let total= (bill * serviceQual) / numOfPeople;
+total = total.toFixed(2);
+document.getElementById('tip'). innerHTML = total;
+document.getElementById("totalTip"). style.display = "block"
+
+
 
 }
 
